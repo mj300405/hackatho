786ca857386561d423b50140ea7287d7f57a9c4c
Jakub Barylak <kubas2001@poczta.fm>
2024-11-30 22:58:52 +0100

    Added user type

---
+++ frontend/lib/types.ts
@@ -0,0 +1,13 @@
+export type User = {
+  id: number;
+  username: string;
+  email: string;
+  age: number;
+  location: string;
+  personality_type: string;
+  available_time: number;
+  budget_preference: string;
+  profile_completed: boolean;
+  coins: number;
+  exp: number;
+};
