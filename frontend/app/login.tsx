aba248aec3e73d0f2fa74bfc6150ce6ddecef23d
Jakub Barylak <kubas2001@poczta.fm>
2024-11-30 20:47:01 +0100

    Working login 🔥🔥🔥🔥🔥

--- frontend/app/login.tsx
+++ frontend/app/login.tsx
@@ -1,14 +1,20 @@
 import { View, Text, Button } from "react-native";
 import Input from "@/components/forms/Input";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { axiosContext, AxiosContextType } from "@/lib/axios";
 import axios, { AxiosError, AxiosResponse } from "axios";
 
 export default function Register() {
+  // Use state to store the values of the input fields
   const [username, setUserName] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
+  // Use state to store the error message
   const [error, setError] = useState("");
 
+  // Context to set the tokens
+
+  const authContext = useContext(axiosContext) as AxiosContextType;
   const handleLogin = () => {
     const requestBody = {
       username,
@@ -16,11 +22,19 @@ export default function Register() {
     };
 
     axios
-      .post(`${process.env.EXPO_PUBLIC_SERVER_URL}/api/token/`, requestBody)
-      .then((response: AxiosResponse) => {})
+      .post(
+        `http://${process.env.EXPO_PUBLIC_SERVER_URL}/api/auth/token/`,
+        requestBody,
+      )
+      .then((response: AxiosResponse) => {
+        const { access, refresh } = response.data;
+        authContext.setToken(access);
+        authContext.setRefreshToken(refresh);
+      })
       .catch((e) => {
         if (e instanceof AxiosError) {
           setError(e.response?.data);
+          console.error(e.toJSON());
         }
       });
   };
@@ -28,10 +42,12 @@ export default function Register() {
   return (
     <View className="bg-white px-2 h-full">
       <Text className="text-center">LOGIN</Text>
-      {error !== "" && <Text className="text-center text-red-400">LOGIN</Text>}
+      {error !== "" && (
+        <Text className="text-center text-red-400">{error}</Text>
+      )}
       <Input label="Login" value={username} onChangeText={setUserName} />
       <Input label="Password" value={password} onChangeText={setPassword} />
-      <View className="h-10" /> {/* Spacer */}
+      <View className="h-10" />
       <Button title="Login" onPress={handleLogin} />
     </View>
   );
