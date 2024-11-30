62d21a4b2b5ff61ace9138be8e8830b36f885405
Michał Jagoda <91846145+mj300405@users.noreply.github.com>
2024-11-30 17:01:18 +0100

    Backend-basics (#6)

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import get_user_model
+from core.serializers import RegisterSerializer, UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+class UserDetailView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
