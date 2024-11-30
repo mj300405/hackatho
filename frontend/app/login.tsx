be2b43514d72f6e1691834b711fa7269aa566ae5
Jakub Barylak <kubas2001@poczta.fm>
2024-11-30 20:02:00 +0100

    Change styles in login form

--- frontend/app/login.tsx
+++ frontend/app/login.tsx
@@ -1,15 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import Input from "@/components/forms/Input";
 import { useState } from "react";
+import axios, { AxiosError, AxiosResponse } from "axios";
 
 export default function Register() {
-  const [userName, setUserName] = useState<string>("");
+  const [username, setUserName] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    const requestBody = {
+      username,
+      password,
+    };
+
+    axios
+      .post(`${process.env.EXPO_PUBLIC_SERVER_URL}/api/token/`, requestBody)
+      .then((response: AxiosResponse) => {})
+      .catch((e) => {
+        if (e instanceof AxiosError) {
+          setError(e.response?.data);
+        }
+      });
+  };
+
   return (
-    <View className="bg-white">
+    <View className="bg-white px-2 h-full">
       <Text className="text-center">LOGIN</Text>
-      <Input label="Login" value={userName} onChangeText={setUserName} />
+      {error !== "" && <Text className="text-center text-red-400">LOGIN</Text>}
+      <Input label="Login" value={username} onChangeText={setUserName} />
       <Input label="Password" value={password} onChangeText={setPassword} />
+      <View className="h-10" /> {/* Spacer */}
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
 }
