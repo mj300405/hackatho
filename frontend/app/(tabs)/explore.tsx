b0be5228dffbe52be4ab39dc5d4f5ec4e8a89a08
Piotr Marcol <52676616+p-marcol@users.noreply.github.com>
2024-12-01 08:56:26 +0100

    Tv roulette (#17)
    * Baisic login and empty register
    
    * Add tab navigation layout and screens for explore, home, profile, roulette, and social
    
    * tabs
    
    * icons
    
    * compass icon ios
    
    * roulette
    
    * colors
    
    * Remove duplicate import of SafeAreaView in _layout.tsx
    
    * Refactor Explore tab layout and add hobby exploration feature
    
    * Details page
    
    * fetch hobbies
    
    * roulette
    
    ---------
    
    Co-authored-by: Jakub Barylak <kubas2001@poczta.fm>

--- frontend/app/(tabs)/explore.tsx
+++ frontend/app/(tabs)/explore.tsx
@@ -1,10 +1,38 @@
-import { ThemedText } from "@/components/ThemedText";
-import { View } from "react-native";
+import { ScrollView, Text } from "react-native";
+import HobbyCard from "@/components/ui/HobbyCard";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useLayoutEffect, useState, useContext } from "react";
+import { axiosContext, AxiosContextType } from "@/lib/axios";
 
 export default function Explore() {
+  const [hobbies, setHobbies] = useState([]);
+  const { axios } = useContext(axiosContext) as AxiosContextType;
+
+  useLayoutEffect(() => {
+    axios
+      .get(`/api/recommendations/explore/`)
+      .then((res) => {
+        console.log(res.data);
+        setHobbies(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
-    <View>
-      <ThemedText>Explore</ThemedText>
-    </View>
+    <GestureHandlerRootView>
+      <Text className="mt-5 m-3 text-3xl font-black">Explore new hobbies!</Text>
+      <ScrollView
+        contentContainerStyle={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        {hobbies.map((hobby, i) => (
+          <HobbyCard key={i} hobby={hobby} />
+        ))}
+      </ScrollView>
+    </GestureHandlerRootView>
   );
 }
