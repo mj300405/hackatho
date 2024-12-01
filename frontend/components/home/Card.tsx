cfefab66454513352ef2e7783453f95f1b248035
Jakub Barylak <kubas2001@poczta.fm>
2024-12-01 08:26:55 +0100

    Home page Cards

---
+++ frontend/components/home/Card.tsx
@@ -0,0 +1,48 @@
+import { UserHobby } from "@/lib/types";
+import React from "react";
+import { View, Text } from "react-native";
+
+type CardProps = {
+  hobby: UserHobby;
+};
+
+export default function Card(props: CardProps) {
+  return (
+    <View className="bg-white p-4 rounded-lg shadow-md w-full mb-4">
+      <Text className="text-lg font-bold text-gray-800">
+        {props.hobby.hobby.name}
+      </Text>
+      <Text className="text-sm text-gray-600">
+        {props.hobby.hobby.category.name}
+      </Text>
+
+      <View className="mt-2">
+        <Text className="text-sm font-semibold text-gray-700">Status:</Text>
+        <Text className="text-sm text-gray-600">{props.hobby.status}</Text>
+      </View>
+
+      <View className="mt-2">
+        <Text className="text-sm font-semibold text-gray-700">Difficulty:</Text>
+        <Text className="text-sm text-gray-600">
+          {props.hobby.hobby.difficulty_level}
+        </Text>
+      </View>
+
+      <View className="mt-2">
+        <Text className="text-sm font-semibold text-gray-700">
+          Days Active:
+        </Text>
+        <Text className="text-sm text-gray-600">{props.hobby.days_active}</Text>
+      </View>
+
+      <View className="mt-2">
+        <Text className="text-sm font-semibold text-gray-700">
+          Last Activity:
+        </Text>
+        <Text className="text-sm text-gray-600">
+          {new Date(props.hobby.last_activity).toLocaleDateString()}
+        </Text>
+      </View>
+    </View>
+  );
+}
